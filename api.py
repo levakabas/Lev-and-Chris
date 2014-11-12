f1fa1bae92095db5c529dc8b99beb0ac7c5b7d16
levakabas <lakabas15@gmail.com>
2014-11-11 22:22:25 -0500

    basically done

--- api.py
+++ api.py
@@ -1,27 +1,43 @@
-#!/usr/bin/env python 
+from flask import Flask,request,url_for,redirect,render_template,session
+import urllib2, json
 
-import urllib2
-import json
+app=Flask(__name__)
+app.secret_key = 'hi'
 
-tomatoe_api = 'krzzdufmpnnbcv4evzufshcd'
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method=="GET":
+        return render_template("index.html")
+    else:
+        search = request.form['search']
+        button = request.form['b']
+        session['search'] = search
+        if (button == "Clear" or search == None):
+            return render_template("index.html")
+        else:
+            return redirect(url_for('results'))
+
+@app.route("/results")
+def results():
+    query = session.pop('search', None)
+    api_key = 'krzzdufmpnnbcv4evzufshcd'
+    movie = query.replace(' ', '+')
+    url = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=' + api_key + '&q=' + movie + '&page_limit=1'
+    json_obj = urllib2.urlopen(url)
+    data = json.load(json_obj)
+    critic = data['movies'][0]['ratings']['critics_score']
+    audience = data['movies'][0]['ratings']['audience_score']
+    synopsis = data['movies'][0]['synopsis']
+    poster = data['movies'][0]['posters']['profile']
+    release_date = data['movies'][0]['release_dates']['theater']
+    cast = ''
+    for item in data['movies'][0]['abridged_cast']:
+        cast = cast + item['name'] + ' '
+    return render_template("results.html",critic=critic,audience=audience,synopsis=synopsis,poster=poster,release_date=release_date,cast=cast,movie=movie)
 
 
-def tomatoe_search(query): 
-	api_key = tomatoe_api
-	movie = query.replace(' ', '+')
-	url = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=' + api_key + '&q=' + movie + '&page_limit=1'
-	json_obj = urllib2.urlopen(url)
-	data = json.load(json_obj)
-	critic = data['movies'][0]['ratings']['critics_score']
-	audience = data['movies'][0]['ratings']['audience_score']
-	synopsis = data['movies'][0]['synopsis']
-	poster = data['movies'][0]['posters']['profile']
-	release_date = data['movies'][0]['release_dates']['theater']
-	cast = ''
-	for item in data['movies'][0]['abridged_cast']:
-		cast = cast + item['name'] + ' '
-	print critic, audience, synopsis, poster, release_date, cast
 
-tomatoe_search('Skyfall')
-
+if __name__=="__main__":
+    app.debug=True
+    app.run(host="0.0.0.0",port=8008)
 
